e3a7250fc7d99aaca87368fe57883fae73771683
liuxy <liuxiaoyong@yylt.com>
2019-07-31 09:24:34 +0800

    Initial Commit

---
+++ XYAlterController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYAlterController
+//
+//  Created by liuxy on 2019/7/31.
+//  Copyright © 2019 liuxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYAlterController/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XYAlterController
+//
+//  Created by liuxy on 2019/7/31.
+//  Copyright © 2019 liuxy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
